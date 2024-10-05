9a14291ee110be77e7d5e06697574c27b842edab
Vikas Singh <130390596+Vikas-Singh99@users.noreply.github.com>
2024-10-05 11:05:21 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all Js code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)  // code redability should be high
+
+console.log("Vikas")
+
+let name = "Vikas"
+let age = 22
+let isLoeggedIn = false
+let state = null 
+let weight;
+
+// primitive data types in javascript //
+
+// number => 2 to the power  53
+// bigint  = biggest numbers
+// string => ""
+// boolean => true/false 
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof null); //undefined
+console.log(typeof undefined); // object
+
+
+
