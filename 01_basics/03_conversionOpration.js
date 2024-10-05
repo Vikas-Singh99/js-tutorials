a108f917b5f8780608535026e7c09e40471b64d4
Vikas Singh <130390596+Vikas-Singh99@users.noreply.github.com>
2024-10-05 12:17:34 +0000

    conversionOfDatattype

---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,28 @@
+let score = "Vikas"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33ab" => NaN
+// true => 1; false => 0
+
+let isLoeggedIn = "Vikas" 
+
+let booleanIsLoggedIn = Boolean(isLoeggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false; 
+// "Vikas" => true
+
+let someNumber = 33
+
+let strinNumber = String(someNumber)
+console.log(strinNumber)
+console.log(typeof strinNumber)
