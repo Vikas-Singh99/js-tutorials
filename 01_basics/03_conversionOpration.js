4e571df1650f5904c3100fc38ab739543ba4e7f0
Vikas Singh <130390596+Vikas-Singh99@users.noreply.github.com>
2024-10-06 09:33:03 +0000

    Operations

--- 01_basics/03_conversionOpration.js
+++ 01_basics/03_conversionOpration.js
@@ -1,11 +1,11 @@
 let score = "Vikas"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33ab" => NaN
@@ -15,7 +15,7 @@ let isLoeggedIn = "Vikas"
 
 let booleanIsLoggedIn = Boolean(isLoeggedIn)
 
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false; 
@@ -24,5 +24,51 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let strinNumber = String(someNumber)
-console.log(strinNumber)
-console.log(typeof strinNumber)
+// console.log(strinNumber)
+// console.log(typeof strinNumber)
+
+
+// ********************************** Operations *********************************
+
+let value  = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/4)
+// console.log(2%4)
+
+let str1 = "Vikas"
+let str2 = "Deep"
+
+let str = str1 + str2
+// console.log(str);
+
+// console.log(1 + "3")
+//  console.log("2" + 3)
+//  console.log(2 - 3)
+// console.log(2 + 2 + "3")
+// console.log("2" + 3 + 3)
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 =  2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+let x = 5
+let y = x++
+console.log(`x:${x}, y:${y}`)
+
+
+
+//type of conversions
+//https://tc39.es/ecma262/#sec-abstract-operations
